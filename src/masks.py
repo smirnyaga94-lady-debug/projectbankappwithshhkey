008671c690e11ac98c2bdb4446fcf9770ab68d39
Ирина Фатеева <Smirnyaga777@mail.ru@git config --global user.email Smirnyaga777@mail.ru>
2026-06-26 14:25:23 +0500

    add src

---
+++ src/masks.py
@@ -0,0 +1,31 @@
+import textwrap
+
+
+def get_mask_card_number(card_num: str) -> str:
+    """Функция маскировки номера банковской карты"""
+
+    # Проверяем длину и то, состоит ли ввод только из цифр
+    if len(card_num) == 16 and card_num.isdigit():
+        card_num = card_num
+    else:
+        return "Ошибка: введено не число или неверное количество знаков."
+    formated_card_num = " ".join(textwrap.wrap(card_num, 4))
+    hidden_card_num = f"{formated_card_num[0:7]}** **** {formated_card_num[-4:]}"
+    return hidden_card_num
+
+
+def get_mask_account(acc_num: str) -> str:
+    """Функция маскировки номера банковского счета"""
+    # Проверяем длину и то, состоит ли ввод только из цифр
+    if len(acc_num) == 20 and acc_num.isdigit():
+        acc_num = acc_num
+    else:
+        return "Ошибка: введено не число или неверное количество знаков."
+    shown_acc_num = acc_num[-4:]
+    hiden_acc_num = f"**{shown_acc_num}"
+    return hiden_acc_num
+
+
+print(get_mask_card_number(str(input("Введите номер карты (12 цифр): "))))
+print(get_mask_account(str(input("Введите номер счета (16 цифр): "))))
+
