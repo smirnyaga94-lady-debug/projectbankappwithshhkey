27c2e9d8ae12ef232bbb5f28934bf1d2794d1b1c
Ирина Фатеева <Smirnyaga777@mail.ru@git config --global user.email Smirnyaga777@mail.ru>
2026-06-28 15:09:46 +0500

    Masks and data

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,7 @@
 """Импорт функций маскировки номеров"""
 from masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
 def mask_account_card(card_account_name: str) ->str:
     """функция маскиорвки номера счета либо номера карты"""
     card_name, card_number = card_account_name.split()
@@ -11,5 +13,11 @@ def mask_account_card(card_account_name: str) ->str:
     result = f"{card_name} {hidden_num}"
     return result
 
+def get_date(date_str: str) -> str:
+        # Превращаем строку в объект datetime, игнорируя миллисекунды (после точки)
+        dt_obj = datetime.strptime(date_str.split('.')[0], "%Y-%m-%dT%H:%M:%S")
+        # Возвращаем дату в требуемом формате ДД.ММ.ГГГГ
+        return dt_obj.strftime("%d.%m.%Y")
 result = mask_account_card(input("Введите номер и название карты через пробел, или счет и номер счета через пробел:  "))
 print(result)
+print(get_date(input("Введите дату:  ")))
