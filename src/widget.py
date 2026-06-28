7197733f9b31576cb99d7c73a3ed14e39860b5f5
Ирина Фатеева <Smirnyaga777@mail.ru@git config --global user.email Smirnyaga777@mail.ru>
2026-06-28 14:56:02 +0500

    Programm masks ready

---
+++ src/widget.py
@@ -0,0 +1,13 @@
+from masks import get_mask_card_number, get_mask_account
+def mask_account_card(card_account_name: str) ->str:
+    card_name, card_number = card_account_name.split()
+    if "счет" in card_name.lower():
+        hidden_num = get_mask_account(card_number)
+    else:
+        hidden_num = get_mask_card_number(card_number)
+
+    result = f"{card_name}: {hidden_num}"
+    return result
+
+result = mask_account_card(input("Введите номер и название карты через пробел, или счет и номер счета через пробел:  "))
+print(result)
