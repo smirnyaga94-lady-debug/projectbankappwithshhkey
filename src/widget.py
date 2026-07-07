0fe36f9c608bb3e5b3376df88609208661dc931b
Ирина Фатеева <smirnyaga94@gmail.com>
2026-07-07 20:03:04 +0500

    fixed widget name=main

--- src/widget.py
+++ src/widget.py
@@ -49,7 +49,7 @@ def get_date(date_str: str) -> str:
         return "Неверный формат данных"
 
 
-result = mask_account_card(input("Введите номер и название карты через пробел, или счет и номер счета через пробел: "))
 if __name__ == "__main__":
+    result = mask_account_card(input("Введите номер и название карты через пробел, или счет и номер счета через пробел: "))
     print(result)
     print(get_date(input("Введите дату:  ")))
